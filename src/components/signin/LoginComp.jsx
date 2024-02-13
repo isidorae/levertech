dce4b5e87fc41c65d4b789cbf0def6b549a96df0
isidorae <mariaisidoraerrazuriz@gmail.com>
2024-02-13 15:36:36 -0300

    login & register

--- src/components/signin/LoginComp.jsx
+++ src/components/signin/LoginComp.jsx
@@ -1,22 +1,68 @@
 import './signin.css'
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 function LoginComp(){
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const emailRegEx = (/^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/g.test(email))
+
+    const [err, setErr] = useState([])
+
+    function submitLoginForm(e){
+        
+        e.preventDefault()
+
+        if (password == "" || email == ""){
+            return setErr([...err, "Debes rellenar todos los campos."])
+        }
+
+        if (!emailRegEx){
+            return setErr([...err, "Debes ingresar un email válido."])
+        }
+
+        let loginData = {
+            email,
+            password
+        }
+
+        console.log(loginData)
+    }
+
+    useEffect(() => {
+        resetErrorMsgs()
+    }, [err])
+
+    const resetErrorMsgs = () => {
+        let timer;
+        if (err.length > 0) {
+            timer =  setTimeout(() => {
+                setErr([])
+            }, 5000)
+        }
+       return () => clearTimeout(timer)
+    }
+
 
     return(
         <>
         <div className="m-5 d-flex flex-column align-items-center justify-content-center">
             <h1 className="mb-4">Iniciar Sesión</h1>
             <div className="login-form p-5">
-                <form className=" d-flex flex-column align-items-center">
+                {err.length > 0 ? <p className="ms-1 form-err-msg">{ err }</p> : null}
+                <form onSubmit={submitLoginForm} className=" d-flex flex-column align-items-center">
+                    <fieldset className="d-flex flex-column align-items-start">
                     <label className="align-self-start ms-1">Email</label>
-                    <input type="email" className="" placeholder="Ingresa tu correo"/>
+                    <input value={email} onChange={(e)=> setEmail(e.target.value)} type="text" className="" placeholder="Ingresa tu correo"/>
                     <label className="align-self-start mt-2 ms-1">Contraseña</label>
-                    <input type="password" className="" placeholder="Contraseña"/>
+                    <input value={password} onChange={(e)=> setPassword(e.target.value)} type="password" className="" placeholder="Contraseña"/>
                     <button className="sign-btn mt-2">Ingresar</button>
+                    </fieldset>
                 </form>
             </div>
-            <p className="mt-2">¿Aún no tienes una cuenta? <Link to="/registro" className="fst-italic fw-bold">Registrate!</Link></p>
+            <p className="mt-2">¿Aún no tienes una cuenta? <Link to="/registro" className="fst-italic fw-bold">Regístrate!</Link></p>
         </div>
         </>
     )
--- src/components/signin/RegisterComp.jsx
+++ src/components/signin/RegisterComp.jsx
@@ -1,22 +1,115 @@
 import './signin.css'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 function RegisterComp(){
+
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [pass, setPass] = useState("")
+    const [confirm, setConfirm] = useState("")
+
+    function submitRegisterForm(e) {
+        e.preventDefault()
+
+        if (nameErr.length > 0 || emailErr.length > 0 || passwordErr.length > 0 || confirmErr.length > 0) {
+            return
+        }
+
+        if (pass != confirm){
+            return console.log("contraseñas deben coincidir.")
+        }
+
+        const registerData = {
+            name,
+            email,
+            pass
+        }
+
+        console.log(registerData)
+
+    }
+
+    // errors
+    const [nameErr, setNameErr] = useState([])
+    const [emailErr, setEmailErr] = useState([])
+    const [passwordErr, setPasswordErr] = useState([])
+    const [confirmErr, setConfirmErr] = useState([])
+
+    const emailRegEx = (/^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/g.test(email))
+
+    function onBlurErrEvent(value){
+        if (value == "name") {
+            if (name == "") {
+                return setNameErr(["Porfavor ingresa tu nombre. 👍🏼"])
+            } else {
+                return setNameErr([])
+            }
+        }
+
+        if (value == "email") {
+            if (!emailRegEx || email == "") {
+                return setEmailErr(["Debes ingresar un email válido."])
+            } else {
+                return setEmailErr([])
+            }
+        }
+
+        if (value == "password") {
+            if (pass.length < 3 || pass == "") {
+                return setPasswordErr(["Tu contraseña debe tener al menos 4 caracteres."])
+            } else {
+                return setPasswordErr([])
+            }
+        }
+
+        if (value == "confirm") {
+            if (confirm != pass|| confirm == "") {
+                return setConfirmErr(["Las contraseñas deben coincidir."])
+            } else {
+                return setConfirmErr([])
+            }
+        }
+    }
 
     return(
         <>
         <div className="m-5 d-flex flex-column align-items-center justify-content-center">
             <h1 className="mb-4">Regístrate</h1>
-            <div className="login-form p-5">
-                <form className=" d-flex flex-column align-items-center">
-                    <label className="align-self-start ms-1">Nombre</label>
-                    <input type="text" className="" placeholder="Ingresa tu nombre"/>
-                    <label className="align-self-start ms-1">Email</label>
-                    <input type="email" className="" placeholder="Ingresa tu correo"/>
-                    <label className="align-self-start mt-2 ms-1">Contraseña</label>
-                    <input type="password" className="" placeholder="Contraseña"/>
-                    <label className="align-self-start mt-2 ms-1">Confirmar contraseña</label>
-                    <input type="confirm" className="" placeholder="Contraseña"/>
+            <div className="register-form p-5">
+                <form onSubmit={submitRegisterForm} className=" d-flex flex-column align-items-center">
+                    <fieldset className="d-flex flex-column align-items-start">
+                        <label className="align-self-start ms-1">Nombre</label>
+                        <input value={name} onChange={(e) => setName(e.target.value)} onBlur={()=> onBlurErrEvent("name")} type="text" className="" placeholder="Ingresa tu nombre"/>
+                        {nameErr.length > 0
+                        ? <span className="ms-1 form-err-msg">{nameErr}</span>
+                        : null
+                        }
+                    </fieldset>
+                    <fieldset className="d-flex flex-column align-items-start">
+                        <label className="align-self-start ms-1">Email</label>
+                        <input value={email} onChange={(e) => setEmail(e.target.value)} onBlur={()=> onBlurErrEvent("email")} type="email" className="" placeholder="Ingresa tu correo"/>
+                        {emailErr.length > 0
+                        ? <span className="ms-1 form-err-msg">{emailErr}</span>
+                        : null
+                        }
+                    </fieldset>
+                    <fieldset className="d-flex flex-column align-items-start">
+                        <label className="align-self-start mt-2 ms-1">Contraseña</label>
+                        <input value={pass} onChange={(e) => setPass(e.target.value)} onBlur={()=> onBlurErrEvent("password")} type="password" className="" placeholder="Contraseña"/>
+                        {passwordErr.length > 0
+                        ? <span className="ms-1 form-err-msg">{passwordErr}</span>
+                        : null
+                        }
+                    </fieldset>
+                    <fieldset className="d-flex flex-column align-items-start">
+                        <label className="align-self-start mt-2 ms-1">Confirmar contraseña</label>
+                        <input value={confirm} onChange={(e) => setConfirm(e.target.value)} onBlur={()=> onBlurErrEvent("confirm")} type="password" className="" placeholder="Contraseña"/>
+                        {confirmErr.length > 0
+                        ? <span className="ms-1 form-err-msg">{confirmErr}</span>
+                        : null
+                        }
+                    </fieldset>
                     <button className="sign-btn mt-2">Registrar</button>
                 </form>
             </div>
